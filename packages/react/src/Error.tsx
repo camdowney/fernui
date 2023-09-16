8577efea9bd3199c93eb52733208ba0fb24980e8
Cameron Downey <camadowney@gmail.com>
2023-09-16 16:14:50 -0500

    react: Export Error component

--- packages/react/src/Error.tsx
+++ packages/react/src/Error.tsx
@@ -3,13 +3,15 @@ import { cn } from '@fernui/react-util'
 import { warning } from './icons'
 import Icon from './Icon'
 
+export interface ErrorProps {
+  text: string
+  className?: string
+}
+
 export default function Error({
   text,
   className,
-}: {
-  text: string
-  className?: string
-}) {
+}: ErrorProps) {
   return (
     <div className='fui-field-error'>
       <Icon i={warning} className={cn('fui-field-error-icon', className)} />
