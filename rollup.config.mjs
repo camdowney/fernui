bc20f635bc5a1f1ba851b686559e814a07324de9
Cameron Downey <camadowney@gmail.com>
2023-10-11 10:51:31 -0500

    Upgrade deps

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -41,8 +41,8 @@ export default [
   // ...config('icons', { flavor: 'vanilla' }),
   // ...config('util', { flavor: 'vanilla' }),
   // ...config('react-core-util', { flavor: 'vanilla' }),
-  ...config('react-util', { flavor: 'vanilla' }),
-  // ...config('react', { flavor: 'react' }),
+  // ...config('react-util', { flavor: 'vanilla' }),
+  ...config('react', { flavor: 'react' }),
   // ...config('react-native-util', { flavor: 'vanilla' }),
   // ...config('react-native', { flavor: 'react' }),
 ]
