2aac903456cd3d6caaad36eed2d5609e156af5cc
Cameron Downey <camadowney@gmail.com>
2023-12-14 12:40:41 -0600

    Create api-util

---
+++ packages/api-util/README.md
@@ -0,0 +1,2 @@
+# FernUI / API Utilites
+A library of general-purpose utilities for working with APIs.
---
+++ packages/api-util/src/index.ts
@@ -0,0 +1,22 @@
+import { KeyObject } from '@fernui/util'
+
+export interface MailHeaderData {
+  to?: string | string[]
+  cc?: string | string[]
+  bcc?: string | string[]
+  replyTo?: string | string[]
+  subject: string
+}
+
+export type MailHeaders = MailHeaderData | ((values: KeyObject) => MailHeaderData)
+
+export interface MailData extends MailHeaderData {
+  from: string
+  html: string
+  text?: string
+}
+
+export interface UploadData {
+  name: string
+  data: string
+}
