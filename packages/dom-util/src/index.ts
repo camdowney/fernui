3137a9327d1d04d5ef1e4717adb48802f6e799ea
Cameron Downey <camadowney@gmail.com>
2024-03-14 12:40:39 -0500

    v1.8.0

--- packages/dom-util/src/index.ts
+++ packages/dom-util/src/index.ts
@@ -4,6 +4,13 @@ export const st = (selector: string, smooth?: boolean) =>
   (document.querySelector(selector) ?? document.body)
     .scrollIntoView({ behavior: smooth ? 'smooth' : 'auto' })
 
+export const navigateBack = (path?: string) => {
+  if (!path || document.referrer.endsWith(path) || document.referrer.includes(`${path}?`))
+    return window.history.back()
+
+  window.location.href = path
+}
+
 export const downloadFile = ({
   name,
   content,
